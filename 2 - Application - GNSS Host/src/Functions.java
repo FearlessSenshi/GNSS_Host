9a2c3c676c04019800117031925b419e405d8073
Latnilakam <Latnilakam@LATN1LAKAM-PC>
2023-08-15 05:30:37 +0800

    GNSS Host Application Committed Changes

---
+++ 2 - Application - GNSS Host/src/Functions.java
@@ -0,0 +1,355 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Functions implements Runnable, KeyListener{
+	MainApp gui;
+	Thread t1;
+	Thread t2;
+	Thread t3;
+	
+	// Host Variables
+	public ServerSocket ss;
+	public Socket cs;
+	public int serverPort;
+	
+	public int hostPasscode;
+	public String hostIP;
+	public String hostName;
+	public final int MIN_PORT_RANGE = 1;
+	public final int MAX_PORT_RANGE = 65536;
+	public boolean unlocked = true;
+	public boolean autoControl = false;
+	
+	// Client Variables
+	public String clientIP;
+	public String clientName;
+	public boolean clientVerified = false;
+	private int attempts = 3;
+	
+	// Get Output
+	private InputStream is;
+	private BufferedReader br;
+	
+	// Send Input
+	private OutputStream out;
+	private PrintWriter pw;
+	
+	public Functions() {
+		gui = new MainApp();
+		
+		gui.createBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					createHost();
+				} catch (UnknownHostException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				showAuthPanel();
+			}
+		});
+		
+		gui.dcButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(clientVerified) {
+					try {
+						disconnect();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				else {
+					System.out.println("[!] No access.");
+				}
+			}
+		});
+	}
+	
+	public static void main(String[] args) {
+		Functions f = new Functions();
+		f.gui.setVisible(true);
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				if(!clientVerified) {
+					// Host established connection to client
+					clientVerified = false;
+					serverPort = generatePort();
+					ss = new ServerSocket(serverPort);
+					System.out.println("Waiting for client connection...");
+					gui.clientConnectStatus.setText("Status: Waiting for client connection...");
+			        cs = ss.accept();
+			        if(cs.isConnected()) {
+			        	is = cs.getInputStream();
+			    		br = new BufferedReader(new InputStreamReader(is));
+			    		out = cs.getOutputStream();
+			    		pw = new PrintWriter(out,true);
+			        }
+			        while(true) {
+			        	// Get Passcode
+			    		String output = "";
+			    		String senderAddress;
+			    		try {
+							// Check if client is connected
+							if (cs.isConnected()) {
+								gui.clientConnectStatus.setText("Status: Awaiting client verification...");
+								// Verify if the client is also using the client app
+								output = br.readLine();
+								System.out.println(output);
+								// This condition checks the client's input for the secret passphrase "connectVerify<passcode>"
+								if (output.equals("connectVerify" + hostPasscode)) {
+									System.out.println("Client Verified!");
+									gui.cardLayout.show(gui.container, "connectStatusPanel");
+									gui.hostIPHostName.setText(hostIP + " - " + "Port: " + serverPort + " - " + hostName);
+									pw.println("gnssVerified" + hostPasscode);
+									clientVerified = true;
+									runInputListener(br);
+									break;
+								} else {
+									// Decrease attempt by -1
+									System.out.println("Incorrect Passcode! ");
+									attempts--;
+									System.out.println(attempts);
+
+									// If attempt ran out to 0, disconnect connecting client
+									if (attempts <= 0) {
+										cs.close();
+										ss.close();
+										is.close();
+										out.close();
+										pw.close();
+										br.close();
+										break;
+									}
+								}
+							}
+							// if client is not connected...
+							else {
+								runOutputSender(pw, "gnssHostDisconnected");
+							}
+						} 
+						catch (IOException e) {
+							// Disconnect the socket and all streams
+							e.printStackTrace();
+							cs.close();
+							ss.close();
+							is.close();
+							out.close();
+							br.close();
+							pw.close();
+							break;
+						}
+			        }
+				}
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+	
+	void createHost() throws UnknownHostException {
+		InetAddress inetAddress = InetAddress.getByName("localhost");
+		hostIP = inetAddress.getLocalHost().getHostAddress();
+		hostName = inetAddress.getLocalHost().getHostName();
+		gui.clientConnectStatus.setText("Status: Waiting client connection...");
+		t1 = new Thread(this);
+		t1.start();
+	}
+	
+	void showAuthPanel() {
+		gui.cardLayout.show(gui.container, "authPanel");
+		gui.clientConnectStatus.setText("Status: Waiting for client connection...");
+		gui.hostIPLabel.setText(getHostDetails());
+		gui.generatedPasscode.setText(String.valueOf(generatePasscode()));
+	}
+	
+	// Host Functions
+	
+	private String getHostDetails() {
+		return hostIP + " - " + "Port: " + serverPort + " - " + hostName;
+	}
+	
+	private int generatePort() {
+		// return (int)(Math.random()*(MAX_PORT_RANGE-MIN_PORT_RANGE))+MIN_PORT_RANGE;
+		return 1;
+	}
+	
+	private int generatePasscode() {
+		hostPasscode = (int)(Math.random()*(100000-10000))+10000;
+		return hostPasscode;
+	}
+	
+	private boolean waitForPasscode(String output) {
+		if (output.equals("verifyConnection" + hostPasscode)) {
+			clientVerified = true;
+			gui.cardLayout.show(gui.container, "connectStatusPanel");
+			gui.hostIPHostName.setText(hostIP + " - " + "Port: " + serverPort + " - " + hostName);
+			System.out.println("Client connection verified!");
+			return true;
+		} 
+		else {
+			System.out.println("Incorrect Passcode! ");
+			attempts--;
+			System.out.println(attempts);
+			return false;
+		}
+	}
+	
+	public void runInputListener(BufferedReader br) {
+		
+		// InputStream
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					try {
+						String output = br.readLine();
+						if(clientVerified) {
+							command(output);
+							System.out.println(output);
+						}
+						
+					} catch (IOException e) {
+						try {
+							br.close();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						e.printStackTrace();
+						break;
+					}
+				}
+			}
+		};
+		
+		t2 = new Thread(runnable);
+		t2.start();
+	}
+	
+	public void runOutputSender(PrintWriter pw, String message) {
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				pw.println(message);
+			}
+			
+		};
+		t3 = new Thread(runnable);
+		t3.start();
+	}
+	
+	public void command(String cmd) throws IOException {
+		if(clientVerified) {
+			switch(cmd) {
+			// Lock PC
+			case "1":
+				lockPC();
+				break;
+			// Unlock PC
+			case "2":
+				unlockPC();
+				break;
+			// Client Direct Disconnection
+			case "3":
+				unlockPC();
+				disconnect();
+			// Switch Security Mode
+			case "toggleControlMode":
+				setControlMode();
+				break;
+			// Host Direct Disconnection
+			case "disconnect":
+				disconnect();
+				break;
+			default:
+				System.out.println("[!] Unknown command.");
+			}
+		}
+		else {
+			System.out.println("[!] No access.");
+		}
+	}
+	
+	private void lockPC() {
+		System.out.println("[!] Locking PC!");
+		gui.securityStatusLabel.setText("Security Status: Locked");
+		// Locks pc (shows whitescreen that cannot be exited or moved, some hotkeys are disabled)
+		gui.w.setVisible(true);
+		// gui.w.setAlwaysOnTop(true); DO NOT UNCOMMENT, UNLESS A FUNCTION IS CREATED TO DESTROY THE APP
+	}
+	private void unlockPC() {
+		System.out.println("[!] Unlocking PC!");
+		gui.securityStatusLabel.setText("Security Status: Unlocked");
+		gui.w.setVisible(false);
+	}
+	private void setControlMode() {
+		System.out.println("[!] Control mode set!");
+		if(!autoControl) {
+			autoControl = true;
+			gui.controlModeLabel.setText("Control Mode: Automatic");
+		}
+		else {
+			autoControl = false;
+			gui.controlModeLabel.setText("Control Mode: Manual");
+		}
+	}
+	private void disconnect() throws IOException {
+		System.out.println("[!] Disconnecting!");
+		clientVerified = false;
+		cs.close();
+		ss.close();
+		is.close();
+		out.close();
+		br.close();
+		pw.close();
+		gui.cardLayout.show(gui.container, "hostPanel");
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
---
+++ 2 - Application - GNSS Host/src/MainApp.java
@@ -0,0 +1,216 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JWindow;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.SwingConstants;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import java.awt.CardLayout;
+import javax.swing.border.BevelBorder;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.awt.event.ActionEvent;
+
+import java.util.*;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+public class MainApp extends JFrame {
+
+	public JPanel contentPane;
+	
+	public JPanel createHostPanel;
+	public JLabel titleLabel;
+	public JLabel dialog1;
+	public JButton createBtn;
+	
+	public JPanel authPanel;
+	public JLabel titleLabel_1;
+	public JLabel hostIPLabel;
+	public JLabel clientConnectStatus;
+	public JLabel generatedPasscode;
+	
+	public JPanel connectStatusPanel;
+	public JLabel hostIPHostName;
+	public JButton dcButton;
+	
+	public CardLayout cardLayout;
+	public Container container;
+	
+	public boolean clientConnected = false;
+	public JLabel securityStatusLabel;
+	public JLabel controlModeLabel;
+	
+	public JWindow w;
+	public JLabel windowLockLabel;
+	
+	/**
+	 * Create the frame.
+	 */
+	
+	public MainApp() {
+		
+		// Layout for the container
+		cardLayout = new CardLayout(0, 0);
+
+		// Container for all panels (uses CardLayout)
+		container = getContentPane();
+		container.setLayout(cardLayout);
+
+		setTitle("GeoNetwork Security System");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 600);
+		setLocationRelativeTo(null);
+		setResizable(false);
+
+		container.setBackground(new Color(128, 255, 128));
+		container.setBounds(0, 0, 584, 561);
+		// contentPane.add(container);
+		container.setLayout(cardLayout);
+
+		createHostPanel = new JPanel();
+		createHostPanel.setBackground(new Color(240, 240, 240));
+		container.add(createHostPanel, "hostPanel");
+		createHostPanel.setLayout(null);
+
+		titleLabel = new JLabel("GeoNetwork Security System (Host)");
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel.setBounds(10, 122, 564, 37);
+		titleLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
+		createHostPanel.add(titleLabel);
+
+		dialog1 = new JLabel("Create Host Connection");
+		dialog1.setHorizontalAlignment(SwingConstants.CENTER);
+		dialog1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		dialog1.setBounds(10, 264, 564, 37);
+		createHostPanel.add(dialog1);
+
+		createBtn = new JButton("CREATE");
+		createBtn.setBorderPainted(false);
+		createBtn.setBackground(new Color(255, 128, 128));
+		createBtn.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		createBtn.setFont(new Font("Tahoma", Font.BOLD, 25));
+		createBtn.setFocusable(false);
+		createBtn.setBounds(199, 312, 167, 53);
+		createHostPanel.add(createBtn);
+
+		authPanel = new JPanel();
+		authPanel.setBackground(new Color(128, 255, 255));
+		container.add(authPanel, "authPanel");
+		authPanel.setLayout(null);
+
+		titleLabel_1 = new JLabel("GeoNetwork Security System (Host)");
+		titleLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		titleLabel_1.setFont(new Font("Tahoma", Font.BOLD, 30));
+		titleLabel_1.setBounds(10, 122, 564, 37);
+		authPanel.add(titleLabel_1);
+
+		JLabel lblHostConnectionCreated = new JLabel("Host Connection Created.");
+		lblHostConnectionCreated.setHorizontalAlignment(SwingConstants.CENTER);
+		lblHostConnectionCreated.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblHostConnectionCreated.setBounds(10, 170, 564, 37);
+		authPanel.add(lblHostConnectionCreated);
+
+		hostIPLabel = new JLabel("Encode (IP Address + Host Name) in this label.");
+		hostIPLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		hostIPLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		hostIPLabel.setBounds(10, 196, 564, 37);
+		authPanel.add(hostIPLabel);
+
+		clientConnectStatus = new JLabel("Status: Encode status here...");
+		clientConnectStatus.setHorizontalAlignment(SwingConstants.CENTER);
+		clientConnectStatus.setFont(new Font("Tahoma", Font.BOLD, 20));
+		clientConnectStatus.setBounds(10, 279, 564, 37);
+		authPanel.add(clientConnectStatus);
+
+		JLabel lblInYourClient = new JLabel("In your client app,");
+		lblInYourClient.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInYourClient.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblInYourClient.setBounds(10, 395, 564, 26);
+		authPanel.add(lblInYourClient);
+
+		JLabel lblPleaseTypeThe = new JLabel("please type the passcode to verify connection to this PC");
+		lblPleaseTypeThe.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPleaseTypeThe.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblPleaseTypeThe.setBounds(10, 418, 564, 26);
+		authPanel.add(lblPleaseTypeThe);
+
+		JLabel lblPasscode = new JLabel("Passcode: ");
+		lblPasscode.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPasscode.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblPasscode.setBounds(180, 443, 138, 37);
+		authPanel.add(lblPasscode);
+
+		generatedPasscode = new JLabel("Encode passcode here");
+		generatedPasscode.setFont(new Font("Tahoma", Font.BOLD, 20));
+		generatedPasscode.setBounds(302, 443, 241, 37);
+		authPanel.add(generatedPasscode);
+
+		connectStatusPanel = new JPanel();
+		connectStatusPanel.setBackground(new Color(255, 255, 128));
+		container.add(connectStatusPanel, "connectStatusPanel");
+		connectStatusPanel.setLayout(null);
+
+		JLabel lblConnected = new JLabel("Connected.");
+		lblConnected.setBounds(10, 96, 564, 25);
+		lblConnected.setHorizontalAlignment(SwingConstants.CENTER);
+		lblConnected.setFont(new Font("Tahoma", Font.BOLD, 20));
+		connectStatusPanel.add(lblConnected);
+
+		hostIPHostName = new JLabel("Encode Host IP/Hostname here");
+		hostIPHostName.setHorizontalAlignment(SwingConstants.CENTER);
+		hostIPHostName.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		hostIPHostName.setBounds(10, 132, 564, 25);
+		connectStatusPanel.add(hostIPHostName);
+		
+		JLabel disconnectFromClient = new JLabel("Disconnect from client");
+		disconnectFromClient.setHorizontalAlignment(SwingConstants.CENTER);
+		disconnectFromClient.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		disconnectFromClient.setBounds(10, 359, 564, 25);
+		connectStatusPanel.add(disconnectFromClient);
+		
+		dcButton = new JButton("DISCONNECT");
+		dcButton.setFocusable(false);
+		dcButton.setForeground(new Color(255, 255, 255));
+		dcButton.setBackground(new Color(255, 0, 0));
+		dcButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+		dcButton.setBorderPainted(false);
+		dcButton.setBounds(190, 395, 201, 57);
+		connectStatusPanel.add(dcButton);
+		
+		securityStatusLabel = new JLabel("Security Status: Unlocked");
+		securityStatusLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		securityStatusLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		securityStatusLabel.setBounds(10, 226, 564, 25);
+		connectStatusPanel.add(securityStatusLabel);
+		
+		controlModeLabel = new JLabel("Control Mode: Manual");
+		controlModeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		controlModeLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		controlModeLabel.setBounds(10, 262, 564, 25);
+		connectStatusPanel.add(controlModeLabel);
+		
+		w = new JWindow();
+		w.getContentPane().setLayout(new BorderLayout());
+		Dimension scSize = Toolkit.getDefaultToolkit().getScreenSize();
+		w.setSize((int)scSize.getWidth(),(int)scSize.getHeight());
+		w.setLocationRelativeTo(null);
+		
+		windowLockLabel = new JLabel("Your PC is locked.");
+		windowLockLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		windowLockLabel.setFont(new Font("Tahoma", Font.BOLD, 50));
+		w.getContentPane().add(windowLockLabel, BorderLayout.CENTER);
+	}
+}
